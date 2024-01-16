b30ed3dea8743b9ac3b41df4bfa4aad8d6c78baa
meenalagarwaal <ameenal0697@gmail.com>
2024-01-16 21:50:20 +0530

    BB-6:update package and index file

--- bingebuy/src/index.js
+++ bingebuy/src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './store/store';
 import { ThemeProvider } from "./context/ThemeContext";
 import App from './App';
 
-const root = document.getElementById('root') || document.createElement('div');
-const createRoot = ReactDOM.createRoot(root);
-
-createRoot.render(
+import { render } from 'react-dom'; 
+import './index.css';
+const root = document.getElementById('root'); 
+render(
   <ThemeProvider>
-    <Provider store={store}>
-    <App  style={{backgroundColor:'#728BA9'}} data-testid="your-app-test-id" />
+  <Provider store={store}>
+  <App  style={{backgroundColor:'#728BA9'}} data-testid="your-app-test-id" />
   </Provider>
- </ThemeProvider>
+  </ThemeProvider>,
+  root
 );
